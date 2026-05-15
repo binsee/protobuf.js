f4c4385b3536b3276d4d498f30de254642d3d63f
dcodeIO <dcode@dcode.io>
2026-05-15 02:33:17 +0200

    docs: Share JSDoc patch with API docs (#2252)

--- src/util/pool.js
+++ src/util/pool.js
@@ -16,7 +16,7 @@ module.exports = pool;
  * @param {number} start Start offset
  * @param {number} end End offset
  * @returns {Uint8Array} Buffer slice
- * @this {Uint8Array}
+ * @this Uint8Array
  */
 
 /**
